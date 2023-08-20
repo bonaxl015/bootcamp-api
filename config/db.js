37eb4e02eb4c4f7bbb9d617628fd6ea876bcf476
Bon Axl Feeser <bonaxl015@gmail.com>
2023-08-20 13:45:18 +0800

    feat: database connection function

---
+++ config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+const connectDb = async () => {
+  const connect = await mongoose.connect(
+    process.env.MONGO_URI,
+    {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true
+    }
+  )
+  console.log(`MongoDB Connected: ${connect.connection.host}`.cyan.underline.bold)
+}
+
+module.exports = connectDb

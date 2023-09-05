e6e786befe99081154af4ae37e3c6238112a67b8
bonaxl015 <bonaxl015@gmail.com>
2023-09-05 22:53:04 +0800

    feat: fix reset password send email function

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,25 +1,15 @@
 const nodemailer = require('nodemailer')
 
-const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST,
-  port: Number(process.env.SMTP_PORT),
-  auth: {
-    user: process.env.SMTP_EMAIL,
-    pass: process.env.SMTP_PASSWORD
-  }
-})
-
 const sendEmail = async options => {
-  console.log({
-    service: 'gmail',
+  const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: process.env.SMTP_PORT,
-    secure: false,
     auth: {
       user: process.env.SMTP_EMAIL,
       pass: process.env.SMTP_PASSWORD
     }
   })
+
   const message = {
     from: `${process.env.FROM_NAME} <${process.env.FROM_EMAIL}>`,
     to: options.email,
@@ -27,10 +17,9 @@ const sendEmail = async options => {
     text: options.message
   }
 
-  console.log(message)
   const info = await transporter.sendMail(message)
 
-  console.log("Message sent: %s", info.messageId)
+  console.log('Message sent:', info.messageId)
 }
 
 module.exports = {

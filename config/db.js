c8399bb963a96568aff59ce3ff71dccd10ae49e3
bonaxl015 <bonaxl015@gmail.com>
2023-10-13 00:14:21 +0800

    fix: added auto restart function and other error catching functions

--- config/db.js
+++ config/db.js
@@ -1,16 +1,42 @@
 const mongoose = require('mongoose')
 
 const connectDb = async () => {
-  const connect = await mongoose.connect(
-    process.env.MONGO_URI,
-    {
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-      useUnifiedTopology: true
-    }
-  )
-  console.log(`MongoDB Connected: ${connect.connection.host}`.cyan.underline.bold)
+  try {
+    const connect = await mongoose.connect(
+      process.env.MONGO_URI,
+      {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true
+      }
+    )
+    console.log(`MongoDB Connected: ${connect.connection.host}`.cyan.underline.bold)
+  } catch(error) {
+    console.error(error.message)
+  }
 }
+
+// Database connection
+const db = mongoose.connection
+
+db.on('error', error => {
+  console.error(`MongoDB Error: ${error.message}`)
+  process.exit(1)
+})
+
+db.on('disconnected', () => {
+  console.log('Database disconnected')
+  console.log('Reconnecting to database...')
+  connectDb()
+})
+
+// Gracefully close the MongoDB connection when the app is closed
+process.on('SIGINT', () => {
+  mongoose.connection.close(() => {
+    console.log('MongoDB connection closed through app termination')
+    process.exit(0)
+  })
+})
 
 module.exports = connectDb
